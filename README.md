f035fc79ab80a0894efd3391a763c5d564dc6d3f
Raea127 <razodonjrfongngao@gmail.com>
2023-11-17 05:34:33 +0800

    FINAL OUTPUT GROUP MEMBERS IN THE README FILE

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JS-RRK-Website
  Website composed of our knowledge in JS and Web Dev
+
+ Group Members: 
+ Razodon Jr. N. Fongngao - saucasage27
+ Rejyna Mae L. Somongsong - Raea127
+ Kenneth Ilon A. De Guzman - Kieranzo
--- script.js
+++ script.js
@@ -1,69 +1,174 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("HelloButton");
-    const container = document.getElementById("container");
+// script.js
 
-    button.addEventListener("click", function() {
-        const body = document.body;
-        body.style.animation = "none";
-        button.style.display = "none";
-        container.style.display = "block";
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("HelloButton");
+  const container = document.getElementById("container");
+  const calculatorContainer = document.getElementById("calculator-container");
+
+  button.addEventListener("click", function () {
+    const body = document.body;
+    body.style.animation = "";
+    button.style.display = "none";
+    container.style.display = "block";
+    showCalculator();
+  });
+
+  function showCalculator() {
+    calculatorContainer.style.display = "block";
+  }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+const numberButtons = document.querySelectorAll('input[type="button"]');
+const equalsButton = document.getElementById('equals');
+const clearButton = document.getElementById('clear');
+const display = document.getElementById('display');
 
-    var num0 = document.getElementById('number0');
-    num0.addEventListener("click", function() {
-        displayNumber(0);
-    });
+class Calculator {
+  constructor() {
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = undefined;
+    this.display = document.getElementById('display');
+  }
 
-    var num00 = document.getElementById('number00');
-    num00.addEventListener("click", function() {
-        displayNumber("00");
-    });
+  clear() {
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = undefined;
+  }
 
-    var num9 = document.getElementById('number9');
-    num9.addEventListener("click", function() {
-        displayNumber(9);
-    });
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
 
-    var num8 = document.getElementById('number8');
-    num8.addEventListener("click", function() {
-        displayNumber(8);
-    });
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
 
-    var num7 = document.getElementById('number7');
-    num7.addEventListener("click", function() {
-        displayNumber(7);
-    });
+  chooseOperation(operation) {
+    if (this.currentOperand === '') return;
+    if (this.previousOperand !== '') {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = '';
+  }
 
-    var num6 = document.getElementById('number6');
-    num6.addEventListener("click", function() {
-        displayNumber(6);
-    });
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
 
-    var num5 = document.getElementById('number5');
-    num5.addEventListener("click", function() {
-        displayNumber(5);
-    });
+    if (isNaN(prev) || isNaN(current)) return;
 
-    var num4 = document.getElementById('number4');
-    num4.addEventListener("click", function() {
-        displayNumber(4);
-    });
+    switch (this.operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '/':
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
 
-    var num3 = document.getElementById('number3');
-    num3.addEventListener("click", function() {
-        displayNumber(3);
-    });
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
+  }
 
-    var num2 = document.getElementById('number2');
-    num2.addEventListener("click", function() {
-        displayNumber(2);
-    });
+  updateDisplay() {
+    display.value = this.currentOperand;
+  }
+}
 
-    var num1 = document.getElementById('number1');
-    num1.addEventListener("click", function() {
-        displayNumber(1);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  function displayNumber(number) {
+    calculator.appendNumber(number);
+    calculator.updateDisplay();
+  }
+
+  var num0 = document.getElementById('number0');
+  num0.addEventListener("click", function () {
+    displayNumber(0);
+  });
+
+  var num00 = document.getElementById('number00');
+  num00.addEventListener("click", function () {
+      displayNumber("00");
+  });
+
+  var num9 = document.getElementById('number9');
+  num9.addEventListener("click", function () {
+      displayNumber(9);
+  });
+
+  var num8 = document.getElementById('number8');
+  num8.addEventListener("click", function () {
+      displayNumber(8);
+  });
+
+  var num7 = document.getElementById('number7');
+  num7.addEventListener("click", function () {
+      displayNumber(7);
+  });
+
+  var num6 = document.getElementById('number6');
+  num6.addEventListener("click", function () {
+      displayNumber(6);
+  });
+
+  var num5 = document.getElementById('number5');
+  num5.addEventListener("click", function () {
+      displayNumber(5);
+  });
+
+  var num4 = document.getElementById('number4');
+  num4.addEventListener("click", function () {
+      displayNumber(4);
+  });
+
+  var num3 = document.getElementById('number3');
+  num3.addEventListener("click", function () {
+      displayNumber(3);
+  });
+
+  var num2 = document.getElementById('number2');
+  num2.addEventListener("click", function () {
+      displayNumber(2);
+  });
+
+  var num1 = document.getElementById('number1');
+  num1.addEventListener("click", function () {
+      displayNumber(1);
+  });
 });
+
+  const calculator = new Calculator();
+  
+  numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.value);
+      calculator.updateDisplay();
+    });
+  });
+
+  equalsButton.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+  });
+
+  clearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+  });
+
+  
